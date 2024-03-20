5a443e2139d20f743cb17a5a4958a43dc1efb39f
jonwestman <jonwestman88@gmail.com>
2024-03-20 17:49:47 +0100

    first test written

---
+++ BlazorPunchCard.Tests/Repository/ApplicationUserRepositoryTests.cs
@@ -0,0 +1,61 @@
+﻿using BlazorPunchCard.Data;
+using BlazorPunchCard.Repositories;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPunchCard.Tests.Repository
+{
+	public class ApplicationUserRepositoryTests
+	{
+		private async Task<ApplicationDbContext> GetDatabaseContext()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+					.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+					.Options;
+			var databaseContext = new ApplicationDbContext(options);
+			databaseContext.Database.EnsureCreated();
+			if (await databaseContext.ApplicationUsers.CountAsync() <= 0)
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					databaseContext.ApplicationUsers.Add(
+						new ApplicationUser()
+						{
+							Id = "1",
+							Name = "Jon Westman",
+							UserName = "jonwestman@hotmail.com",
+							NormalizedUserName = "JONWESTMAN@HOTMAIL.COM",
+							Email = "jonwestman@hotmail.com",
+							NormalizedEmail = "JONWESTMAN@HOTMAIL.COM",
+							EmailConfirmed = true,
+							PhoneNumber = "0701234567"
+						});
+					await databaseContext.SaveChangesAsync();
+				}
+			}
+			return databaseContext;
+		}
+
+		[Fact]
+		public async void ApplicationUserRepository_GetById_ReturnsUser() 
+		{
+			// Arrange
+			var userId = "1";
+			var dbContext = await GetDatabaseContext();
+			var applicationUserRepository = new ApplicationUserRepository(dbContext);
+
+			// Act
+			var result = applicationUserRepository.GetById(userId);	
+
+			// Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(Task<ApplicationUser>));
+		}
+	}
+}
